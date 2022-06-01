a620b6796768ed1ec601b0535797e2c0a31b06fb
gotbash <mammadov@live.co.uk>
2022-06-01 14:00:33 +0200

    test

---
+++ src/test/java/tests/junit/CalculatorTest.java
@@ -0,0 +1,20 @@
+package tests.junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void getSum() {
+    }
+
+    @Test
+    public void getDivide() {
+    }
+
+    @Test
+    public void getMultiple() {
+    }
+}
