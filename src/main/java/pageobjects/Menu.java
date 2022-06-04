f978d05c7285e4e4bb6824543154e3a14def007a
gotbash <mammadov@live.co.uk>
2022-06-04 16:55:58 +0200

    test

--- src/main/java/pageobjects/Menu.java
+++ src/main/java/pageobjects/Menu.java
@@ -7,21 +7,14 @@ import org.openqa.selenium.WebElement;
 public class Menu {
 
     private WebDriver driver;
-    WebElement tickets = driver.findElement(By.id("menu-tickets"));
-    WebElement dashboard = driver.findElement(By.id("menu-dashboard"));
-    WebElement companies = driver.findElement(By.id("menu-companies"));
-    WebElement contacts = driver.findElement(By.id("menu-contacts"));
-    WebElement devices = driver.findElement(By.id("menu-device-list"));
-    WebElement departments = driver.findElement(By.id("menu-departments"));
-    WebElement managers = driver.findElement(By.id("menu-managers"));
-    WebElement categories = driver.findElement(By.id("menu-categories"));
-    WebElement menu_tickets = driver.findElement(By.xpath("//a[@id='menu-tickets']//p"));
-    WebElement menu_dashboard = driver.findElement(By.xpath("//a[@id='menu-dashboard']//p"));
-    WebElement menu_companies = driver.findElement(By.xpath("//a[@id='menu-companies']//p"));
-    WebElement menu_contacts = driver.findElement(By.xpath("//a[@id='menu-contacts']//p"));
-    WebElement menu_devices = driver.findElement(By.xpath("//a[@id='menu-device-list']//p"));
-    WebElement menu_departments = driver.findElement(By.xpath("//a[@id='menu-departments']//p"));
-    WebElement menu_categories = driver.findElement(By.xpath("//a[@id='menu-categories']//p"));
+
+    WebElement menuTickets = driver.findElement(By.xpath("//a[@id='menu-tickets']//p"));
+    WebElement menuDashboard = driver.findElement(By.xpath("//a[@id='menu-dashboard']//p"));
+    WebElement menuCompanies = driver.findElement(By.xpath("//a[@id='menu-companies']//p"));
+    WebElement menuContacts = driver.findElement(By.xpath("//a[@id='menu-contacts']//p"));
+    WebElement menuDevices = driver.findElement(By.xpath("//a[@id='menu-device-list']//p"));
+    WebElement menuDepartments = driver.findElement(By.xpath("//a[@id='menu-departments']//p"));
+    WebElement menuCategories = driver.findElement(By.xpath("//a[@id='menu-categories']//p"));
 
 
 
