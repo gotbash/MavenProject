a92a93d537f37d77a1d79d62bef0046905849130
gotbash <mammadov@live.co.uk>
2022-06-14 21:12:21 +0200

    test

--- src/main/java/config/BaseChromeDriverConfiguration.java
+++ src/main/java/config/BaseChromeDriverConfiguration.java
@@ -1,15 +1,32 @@
 package config;
 
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class BaseChromeDriverConfiguration {
-    @Test
-    public void launchBrowser() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://176.36.27.131:8180/#/");
+    public static WebDriver driver;
+
+    @BeforeSuite
+    public void initializeWebDriver() throws IOException {
+        System.setProperty("webdriver.chrome.driver", "\\src\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        // Implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // To open Gmail site
+        driver.get("https:// www.gmail.com");
+    }
+
+    @AfterSuite
+    public void quitDriver() {
         driver.quit();
     }
 }
